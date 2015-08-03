210d51b4fd851417feff5d6363c99d299767dab5
Florian Zoubek <zoubek@bitandart.at>
2015-08-03 13:21:39 +0200

    adds similarity columns to the patch set history tree

--- plugins/at.bitandart.zoubek.mervin/src/at/bitandart/zoubek/mervin/patchset/history/PatchSetHistoryView.java
+++ plugins/at.bitandart.zoubek.mervin/src/at/bitandart/zoubek/mervin/patchset/history/PatchSetHistoryView.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package at.bitandart.zoubek.mervin.patchset.history;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -32,15 +33,20 @@ import org.eclipse.jface.viewers.TreeViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
 
 import at.bitandart.zoubek.mervin.model.modelreview.ModelReview;
 import at.bitandart.zoubek.mervin.model.modelreview.PatchSet;
 import at.bitandart.zoubek.mervin.review.ModelReviewEditorTrackingView;
+import at.bitandart.zoubek.mervin.util.vis.HSB;
+import at.bitandart.zoubek.mervin.util.vis.NumericColoredColumnLabelProvider;
 import at.bitandart.zoubek.mervin.util.vis.ThreeWayLabelTreeViewerComparator;
 
 /**
@@ -56,12 +62,12 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 	private static final String CONTAINER_NAME_MODEL_DIFFERENCES = "Model Differences";
 
 	private static final String CONTAINER_NAME_DIAGRAM_DIFFERENCES = "Diagram Differences";
-	
+
 	/**
 	 * indicates if all SWT controls and viewers have been correctly set up
 	 */
 	private boolean viewInitialized = false;
-	
+
 	private EContentAdapter patchSetHistoryViewUpdater;
 
 	@Inject
@@ -81,6 +87,8 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 	 */
 	private Composite mainPanel;
 
+	private TreeViewerColumn labelColumn;
+
 	@Inject
 	public PatchSetHistoryView() {
 		patchSetHistoryViewUpdater = new UpdatePatchSetHistoryViewAdapter();
@@ -95,11 +103,9 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 
 		// initialize tree viewer
 
-		historyTreeViewer = new TreeViewer(mainPanel, SWT.SINGLE | SWT.H_SCROLL
-				| SWT.V_SCROLL);
+		historyTreeViewer = new TreeViewer(mainPanel, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL);
 		historyTreeViewer.setComparator(new ViewerComparator());
-		historyTreeViewer
-				.setContentProvider(new PatchSetHistoryContentProvider());
+		historyTreeViewer.setContentProvider(new PatchSetHistoryContentProvider());
 		Tree reviewTree = historyTreeViewer.getTree();
 		reviewTree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		reviewTree.setLinesVisible(false);
@@ -107,99 +113,122 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 
 		// set up all columns of the tree
 
-		// main label column
-		TreeViewerColumn labelColumn = new TreeViewerColumn(historyTreeViewer,
-				SWT.NONE);
+		labelColumn = new TreeViewerColumn(historyTreeViewer, SWT.NONE);
 		labelColumn.getColumn().setResizable(true);
 		labelColumn.getColumn().setMoveable(true);
 		labelColumn.getColumn().setText("Diff");
-		labelColumn.getColumn().setWidth(200);
+		labelColumn.getColumn().setWidth(400);
 
 		DiffNameColumnLabelProvider labelColumnLabelProvider = new DiffNameColumnLabelProvider();
 		labelColumn.setLabelProvider(labelColumnLabelProvider);
 		labelColumn.getColumn().addSelectionListener(
-				new ThreeWayLabelTreeViewerComparator(historyTreeViewer,
-						labelColumn, labelColumnLabelProvider));
-		
-		
-		// TODO create columns per patch set see method below
-		
-		
+				new ThreeWayLabelTreeViewerComparator(historyTreeViewer, labelColumn, labelColumnLabelProvider));
+
 		viewInitialized = true;
 	}
-	
-	private TreeViewerColumn createColumnForPatchSet(TreeViewer historyTreeViewer, PatchSet patchSet){
-		TreeViewerColumn column = new TreeViewerColumn(historyTreeViewer,
-				SWT.NONE);
+
+	/**
+	 * creates a new column in the given tree viewer that contains the history
+	 * value for the given patch set.
+	 * 
+	 * @param historyTreeViewer
+	 *            the {@link TreeViewer} that should contain the color.
+	 * @param patchSet
+	 *            the {@link PatchSet} associated with this column.
+	 * @return the created column
+	 */
+	private TreeViewerColumn createColumnForPatchSet(TreeViewer historyTreeViewer, PatchSet patchSet) {
+		TreeViewerColumn column = new TreeViewerColumn(historyTreeViewer, SWT.NONE);
 		column.getColumn().setResizable(true);
 		column.getColumn().setMoveable(true);
 		column.getColumn().setText(patchSet.getId());
 		column.getColumn().setWidth(50);
-		
-		// TODO add label provider
-		
+
+		// TODO make color configurable
+		column.setLabelProvider(new PatchSetSimilarityHistoryLabelProvider(patchSet, new HSB(205.0f, 0.f, 1.0f),
+				new HSB(205.0f, 0.59f, 0.32f), Display.getCurrent().getSystemColor(SWT.COLOR_WHITE),
+				Display.getCurrent().getSystemColor(SWT.COLOR_BLACK)));
+
 		return column;
-		
+
 	}
 
 	@Override
 	protected void updateValues() {
 		// we cannot update the controls if they are not initialized yet
 		if (viewInitialized) {
-			
+
 			ModelReview currentModelReview = getCurrentModelReview();
-			
-			if(currentModelReview != null){
-			
-				if(!currentModelReview.eAdapters().contains(patchSetHistoryViewUpdater)){
+
+			if (currentModelReview != null) {
+
+				if (!currentModelReview.eAdapters().contains(patchSetHistoryViewUpdater)) {
 					currentModelReview.eAdapters().add(patchSetHistoryViewUpdater);
 				}
-	
+
 				updateHistoryTree(currentModelReview);
 			}
-			
+
 			historyTreeViewer.refresh();
 			mainPanel.redraw();
 			mainPanel.update();
 		}
 
 	}
-	
-	private void updateHistoryTree(ModelReview currentModelReview){
+
+	/**
+	 * updates the history tree data and the columns of the tree.
+	 * 
+	 * @param currentModelReview
+	 *            the model review to retrieve the data from.
+	 */
+	private void updateHistoryTree(ModelReview currentModelReview) {
 
 		EList<PatchSet> patchSets = currentModelReview.getPatchSets();
 		// treat the right patch set as the "active" patch set
 		PatchSet rightPatchSet = currentModelReview.getRightPatchSet();
-		
+
 		List<Object> historyData = new ArrayList<Object>(2);
 
-		if(rightPatchSet != null){
-			
+		if (rightPatchSet != null) {
+
 			List<IPatchSetHistoryEntry<Diff, Double>> modelHistoryEntries = similarityHistoryService
 					.createModelEntries(rightPatchSet, patchSets);
-			historyData.add(new NamedHistoryEntryContainer(
-					CONTAINER_NAME_MODEL_DIFFERENCES, modelHistoryEntries));
-	
+			historyData.add(new NamedHistoryEntryContainer(CONTAINER_NAME_MODEL_DIFFERENCES, modelHistoryEntries));
+
 			List<IPatchSetHistoryEntry<Diff, Double>> diagramHistoryEntries = similarityHistoryService
 					.createDiagramEntries(rightPatchSet, patchSets);
-			historyData.add(new NamedHistoryEntryContainer(
-					CONTAINER_NAME_DIAGRAM_DIFFERENCES, diagramHistoryEntries));
-			
-		}else{
+			historyData.add(new NamedHistoryEntryContainer(CONTAINER_NAME_DIAGRAM_DIFFERENCES, diagramHistoryEntries));
+
+		} else {
 			historyData.add("Please select an active patch set (right side of the comparison is the active patch set)");
+		}
+
+		// update columns
+
+		for (TreeColumn column : historyTreeViewer.getTree().getColumns()) {
+			// remove all columns except the label column
+			if (column != labelColumn.getColumn()) {
+				column.dispose();
+			}
+		}
+
+		// create new columns for each patch set
+		for (PatchSet patchSet : patchSets) {
+			createColumnForPatchSet(historyTreeViewer, patchSet);
 		}
 
 		historyTreeViewer.setInput(historyData);
 	}
-	
-	private class UpdatePatchSetHistoryViewAdapter extends EContentAdapter{
-		
+
+	private class UpdatePatchSetHistoryViewAdapter extends EContentAdapter {
+
 		@Override
 		public void notifyChanged(Notification notification) {
 			super.notifyChanged(notification);
 			updateValues();
 		}
-		
+
 	}
 
 	/**
@@ -244,8 +273,7 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 		@Override
 		public Object[] getChildren(Object parentElement) {
 			if (parentElement instanceof NamedHistoryEntryContainer) {
-				return ((NamedHistoryEntryContainer) parentElement)
-						.getEntries().toArray();
+				return ((NamedHistoryEntryContainer) parentElement).getEntries().toArray();
 			}
 			return new Object[0];
 		}
@@ -280,8 +308,7 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 
 		private String name;
 
-		public NamedHistoryEntryContainer(String name,
-				List<? extends IPatchSetHistoryEntry<?, ?>> entries) {
+		public NamedHistoryEntryContainer(String name, List<? extends IPatchSetHistoryEntry<?, ?>> entries) {
 			this.name = name;
 			this.entries = entries;
 		}
@@ -309,9 +336,8 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 		private AdapterFactoryLabelProvider adapterFactoryLabelProvider;
 
 		public DiffNameColumnLabelProvider() {
-			adapterFactoryLabelProvider = new AdapterFactoryLabelProvider(
-					new ComposedAdapterFactory(EMFCompareRCPPlugin.getDefault()
-							.createFilteredAdapterFactoryRegistry()));
+			adapterFactoryLabelProvider = new AdapterFactoryLabelProvider(new ComposedAdapterFactory(
+					EMFCompareRCPPlugin.getDefault().createFilteredAdapterFactoryRegistry()));
 		}
 
 		@Override
@@ -319,12 +345,12 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 
 			if (element instanceof NamedHistoryEntryContainer) {
 
-				return ((NamedHistoryEntryContainer) element).getName();
+				NamedHistoryEntryContainer container = (NamedHistoryEntryContainer) element;
+				return MessageFormat.format("{0} ({1})", container.getName(), container.entries.size());
 
 			} else if (element instanceof IPatchSetHistoryEntry) {
 
-				Object entryObject = ((IPatchSetHistoryEntry<?, ?>) element)
-						.getEntryObject();
+				Object entryObject = ((IPatchSetHistoryEntry<?, ?>) element).getEntryObject();
 				// delegate to the default EMF compare label provider
 				return adapterFactoryLabelProvider.getText(entryObject);
 
@@ -337,13 +363,62 @@ public class PatchSetHistoryView extends ModelReviewEditorTrackingView {
 
 			if (element instanceof IPatchSetHistoryEntry) {
 
-				Object entryObject = ((IPatchSetHistoryEntry<?, ?>) element)
-						.getEntryObject();
+				Object entryObject = ((IPatchSetHistoryEntry<?, ?>) element).getEntryObject();
 				// delegate to the default EMF compare label provider
 				return adapterFactoryLabelProvider.getImage(entryObject);
 
 			}
 			return super.getImage(element);
+		}
+
+	}
+
+	/**
+	 * A {@link NumericColoredColumnLabelProvider} that provides labels for
+	 * {@link IPatchSetHistoryEntry}s with a {@link Number} as value. Other
+	 * value types and objects will be ignored.
+	 * 
+	 * @author Florian Zoubek
+	 *
+	 */
+	private class PatchSetSimilarityHistoryLabelProvider extends NumericColoredColumnLabelProvider {
+
+		private PatchSet patchSet;
+
+		public PatchSetSimilarityHistoryLabelProvider(PatchSet patchSet, HSB minHSB, HSB maxHSB, Color fgColor1,
+				Color fgColor2) {
+			super(minHSB, maxHSB, fgColor1, fgColor2);
+			this.patchSet = patchSet;
+		}
+
+		@Override
+		public float getMaxValue(Object element) {
+			return 1.0f;
+		}
+
+		@Override
+		public float getMinValue(Object element) {
+			return 0.0f;
+		}
+
+		@Override
+		public float getValue(Object element) {
+			if (element instanceof IPatchSetHistoryEntry) {
+				Object value = ((IPatchSetHistoryEntry<?, ?>) element).getValue(patchSet);
+				if (value instanceof Number) {
+					return ((Number) value).floatValue();
+				}
+			}
+			return 0;
+		}
+
+		@Override
+		public boolean hasValue(Object element) {
+			if (element instanceof IPatchSetHistoryEntry) {
+				Object value = ((IPatchSetHistoryEntry<?, ?>) element).getValue(patchSet);
+				return value instanceof Number;
+			}
+			return false;
 		}
 
 	}
