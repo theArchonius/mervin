59c1d2425b9660cd8ffed20a6f98e02dba79b5a3
Florian Zoubek <zoubek@bitandart.at>
2015-09-25 15:47:48 +0200

    adds overlay figure for figures with rectangular bounds
    - adds an example for ChangeOverlayNodeFigures
    - adds base overlay figure interface
    - adds a FigureListener for linked figures that revalidates the overlay 
      layer
    - adds a change indicator for dimension properties
    - adds a rectangular outline figure
    - adds a default Locator implementation for overlay figures
    - adds an overlay figure for figures with rectangular bounds

---
+++ plugins/at.bitandart.zoubek.mervin.draw2d/src/at/bitandart/zoubek/mervin/draw2d/figures/ChangeOverlayNodeFigure.java
@@ -0,0 +1,528 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Florian Zoubek.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Florian Zoubek - initial API and implementation
+ *******************************************************************************/
+package at.bitandart.zoubek.mervin.draw2d.figures;
+
+import java.util.Collection;
+
+import org.eclipse.draw2d.Border;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.LineBorder;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Insets;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.draw2d.geometry.Vector;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+
+import at.bitandart.zoubek.mervin.draw2d.figures.DimensionPropertyChangeIndicator.DimensionChange;
+import at.bitandart.zoubek.mervin.draw2d.figures.DimensionPropertyChangeIndicator.DimensionProperty;
+
+/**
+ * An {@link IOverlayFigure} with a rectangular outline and indicators for
+ * layout changes. A {@link IChangeTypeStyleAdvisor} can be used to control the
+ * colors for this figure.
+ * 
+ * @author Florian Zoubek
+ *
+ */
+public class ChangeOverlayNodeFigure extends ComposedNodeFigure implements IOverlayFigure {
+
+	/**
+	 * The change types supported by {@link ChangeOverlayNodeFigure}.
+	 * 
+	 * @author Florian Zoubek
+	 *
+	 */
+	public enum ChangeType {
+		ADDITION, DELETION, MODIFICATION, LAYOUT
+	}
+
+	/**
+	 * The diemnsion property change types supported by
+	 * {@link ChangeOverlayNodeFigure}.
+	 * 
+	 * @author Florian Zoubek
+	 *
+	 */
+	public enum DimensionPropertyChangeType {
+		NONE, SMALLER, BIGGER
+	}
+
+	// child figures
+
+	private RectangularChangeOutline outline;
+	private DirectionIndicator directionIndicator;
+	private DimensionPropertyChangeIndicator boundsWidthSizeChangeIndicator;
+	private DimensionPropertyChangeIndicator boundsHeightSizeChangeIndicator;
+
+	// layout properties
+
+	private Insets outlineInsets = new Insets(3);
+	private Dimension defaultDirectionIndicatorSize = new Dimension(35, 35);
+	private Dimension defaultBoundsWidthSizeChangeIndicatorSize = new Dimension(50, 20);
+	private Dimension defaultBoundsHeightChangeIndicatorSize = new Dimension(20, 50);
+
+	private IChangeTypeStyleAdvisor styleAdivsor;
+
+	// change properties
+
+	private Vector moveDirection = null;
+	private ChangeType changeType = ChangeType.ADDITION;
+	private boolean showCommentHint = false;
+	private DimensionPropertyChangeType boundsWidthChangeType = DimensionPropertyChangeType.NONE;
+	private DimensionPropertyChangeType boundsHeightChangeType = DimensionPropertyChangeType.NONE;
+
+	/**
+	 * @param styleAdivsor
+	 *            the style advisor used to obtain the styles for the different
+	 *            change types. Null values are not permitted.
+	 * @param changeType
+	 *            the change type that this figure represents
+	 * @throws IllegalArgumentException
+	 *             if null is passed as a style advisor
+	 */
+	public ChangeOverlayNodeFigure(IChangeTypeStyleAdvisor styleAdivsor, ChangeType changeType)
+			throws IllegalArgumentException {
+		if (styleAdivsor == null) {
+			throw new IllegalArgumentException("A valid style advisor must be specified");
+		}
+		this.styleAdivsor = styleAdivsor;
+		this.changeType = changeType;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see at.bitandart.zoubek.mervin.draw2d.figures.ComposedNodeFigure#
+	 * initializeFigure()
+	 */
+	@Override
+	protected void initializeFigure() {
+		setLayoutManager(null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see at.bitandart.zoubek.mervin.draw2d.figures.ComposedNodeFigure#
+	 * initializeChildren()
+	 */
+	@Override
+	protected void initializeChildren() {
+
+		outline = new RectangularChangeOutline(styleAdivsor.getCommentColorForChangeType(changeType));
+		outline.setAlpha(128);
+		outline.setLineWidthFloat(2f);
+		outline.setBounds(new Rectangle(0, 0, 20, 20));
+		add(outline);
+		if (moveDirection != null) {
+			directionIndicator = new DirectionIndicator(moveDirection);
+		} else {
+			directionIndicator = new DirectionIndicator(new Vector(0, 1));
+			directionIndicator.setVisible(false);
+		}
+		directionIndicator.setOpaque(true);
+		directionIndicator.setBorder(new LineBorder(styleAdivsor.getForegroundColorForChangeType(changeType), 5));
+		directionIndicator.setBounds(Rectangle.SINGLETON.setBounds(20, 0, defaultDirectionIndicatorSize.width,
+				defaultDirectionIndicatorSize.height));
+		add(directionIndicator);
+
+		boundsWidthSizeChangeIndicator = new DimensionPropertyChangeIndicator(DimensionProperty.WIDTH,
+				DimensionChange.BIGGER);
+		boundsWidthSizeChangeIndicator.setOpaque(true);
+		boundsWidthSizeChangeIndicator
+				.setBorder(new LineBorder(styleAdivsor.getForegroundColorForChangeType(changeType), 5));
+		add(boundsWidthSizeChangeIndicator);
+
+		boundsHeightSizeChangeIndicator = new DimensionPropertyChangeIndicator(DimensionProperty.HEIGHT,
+				DimensionChange.BIGGER);
+		boundsHeightSizeChangeIndicator.setOpaque(true);
+		boundsHeightSizeChangeIndicator
+				.setBorder(new LineBorder(styleAdivsor.getForegroundColorForChangeType(changeType), 5));
+		add(boundsHeightSizeChangeIndicator);
+
+	}
+
+	@Override
+	protected void notifyInitializationComplete() {
+		applyChangeStyles();
+		updateDimensionPropertyChangeIndicators();
+		updateOutline();
+	}
+
+	/**
+	 * applies the styles to the child figures based on the current state
+	 */
+	private void applyChangeStyles() {
+
+		Color foregroundColor = styleAdivsor.getForegroundColorForChangeType(changeType);
+		Color backgroundColor = styleAdivsor.getBackgroundColorForChangeType(changeType);
+
+		outline.setForegroundColor(foregroundColor);
+		outline.setBackgroundColor(backgroundColor);
+		outline.setFill(changeType != ChangeType.LAYOUT);
+
+		directionIndicator.setBackgroundColor(foregroundColor);
+		directionIndicator.setForegroundColor(styleAdivsor.getIndicatorColorForChangeType(changeType));
+		Border indicatorBorder = directionIndicator.getBorder();
+		if (indicatorBorder instanceof LineBorder) {
+			((LineBorder) indicatorBorder).setColor(foregroundColor);
+		}
+
+		boundsHeightSizeChangeIndicator.setBackgroundColor(foregroundColor);
+		boundsHeightSizeChangeIndicator.setForegroundColor(styleAdivsor.getIndicatorColorForChangeType(changeType));
+		indicatorBorder = boundsHeightSizeChangeIndicator.getBorder();
+		if (indicatorBorder instanceof LineBorder) {
+			((LineBorder) indicatorBorder).setColor(foregroundColor);
+		}
+
+		boundsWidthSizeChangeIndicator.setBackgroundColor(foregroundColor);
+		boundsWidthSizeChangeIndicator.setForegroundColor(styleAdivsor.getIndicatorColorForChangeType(changeType));
+		indicatorBorder = boundsWidthSizeChangeIndicator.getBorder();
+		if (indicatorBorder instanceof LineBorder) {
+			((LineBorder) indicatorBorder).setColor(foregroundColor);
+		}
+
+	}
+
+	@Override
+	public void updateBoundsfromLinkedFigures(Collection<IFigure> linkedFigures) {
+
+		Rectangle areaToCover = null;
+
+		for (IFigure figure : linkedFigures) {
+			Rectangle linkedFigureBounds = getLocalBounds(figure);
+
+			if (areaToCover == null) {
+				areaToCover = Rectangle.SINGLETON.setBounds(linkedFigureBounds);
+			} else {
+				areaToCover.union(linkedFigureBounds);
+			}
+		}
+
+		// add insets and padding caused by the the line width
+		areaToCover.expand(outlineInsets);
+		double lineWidthSpacing = outline.getLineWidth() * 2.0;
+		areaToCover.expand(lineWidthSpacing, lineWidthSpacing);
+
+		Rectangle outlineBounds = areaToCover.getCopy();
+
+		Rectangle dirIndicatorBounds = new Rectangle(outlineBounds.getTopRight(), defaultDirectionIndicatorSize);
+
+		Rectangle horizontalSideChangeIndicatorBounds = new Rectangle(
+				outlineBounds.getBottom().translate((int) defaultBoundsWidthSizeChangeIndicatorSize.width / -2.0, 0),
+				defaultBoundsWidthSizeChangeIndicatorSize);
+
+		Rectangle verticalSideChangeIndicatorBounds = new Rectangle();
+		verticalSideChangeIndicatorBounds.setSize(defaultBoundsHeightChangeIndicatorSize);
+		if (outlineBounds.height < defaultBoundsHeightChangeIndicatorSize.height + defaultDirectionIndicatorSize.height
+				&& moveDirection != null) {
+
+			// not enough space to display the side change indicator on the same
+			// same side as the direction indicator -> use the other side
+			Point left = outlineBounds.getLeft();
+			verticalSideChangeIndicatorBounds.setLocation(left.x - defaultBoundsHeightChangeIndicatorSize.width,
+					(int) (left.y - defaultBoundsHeightChangeIndicatorSize.height / 2.0));
+
+		} else if (outlineBounds.height < 2
+				* (defaultBoundsHeightChangeIndicatorSize.height / 2.0 + defaultDirectionIndicatorSize.height)
+				&& moveDirection != null) {
+
+			// not enough space to center the side change indicator on the right
+			// side -> add it under the direction indicator
+			verticalSideChangeIndicatorBounds.setLocation(dirIndicatorBounds.getBottomLeft());
+
+		} else {
+
+			// center it on the right side
+			Point right = outlineBounds.getRight();
+			verticalSideChangeIndicatorBounds.setLocation(right.x,
+					(int) (right.y - defaultBoundsHeightChangeIndicatorSize.height / 2.0));
+
+		}
+
+		// obtain the main figure bounds
+		areaToCover.union(dirIndicatorBounds);
+		areaToCover.union(horizontalSideChangeIndicatorBounds);
+		areaToCover.union(verticalSideChangeIndicatorBounds);
+
+		/*
+		 * update the bounds beginning with the parent to avoid potential child
+		 * translation caused by setBounds();
+		 */
+		setBounds(areaToCover);
+		outline.setBounds(outlineBounds);
+		directionIndicator.setBounds(dirIndicatorBounds);
+		boundsHeightSizeChangeIndicator.setBounds(verticalSideChangeIndicatorBounds);
+		boundsWidthSizeChangeIndicator.setBounds(horizontalSideChangeIndicatorBounds);
+
+	}
+
+	/**
+	 * updates all {@link DimensionPropertyChangeIndicator}s of this figure
+	 * based on the current state
+	 */
+	protected void updateDimensionPropertyChangeIndicators() {
+
+		updateDimensionPropertyChangeIndicator(boundsHeightSizeChangeIndicator, boundsHeightChangeType);
+		updateDimensionPropertyChangeIndicator(boundsWidthSizeChangeIndicator, boundsWidthChangeType);
+
+	}
+
+	/**
+	 * updates the outline properties based on the current state
+	 */
+	protected void updateOutline() {
+
+		if (isInitialized()) {
+			outline.setShowCommentHint(showCommentHint);
+		}
+
+	}
+
+	/**
+	 * updates the given indicator based on the given change type.
+	 * 
+	 * @param indicator
+	 *            the indicator to update
+	 * @param changeType
+	 *            the changeType used to update the indicator.
+	 */
+	protected void updateDimensionPropertyChangeIndicator(DimensionPropertyChangeIndicator indicator,
+			DimensionPropertyChangeType changeType) {
+
+		if (isInitialized()) {
+
+			switch (changeType) {
+			case NONE:
+				indicator.setVisible(false);
+				break;
+			case BIGGER:
+				indicator.setVisible(true);
+				indicator.setDimensionChange(DimensionChange.BIGGER);
+				break;
+			case SMALLER:
+				indicator.setVisible(true);
+				indicator.setDimensionChange(DimensionChange.SMALLER);
+				break;
+			}
+		}
+	}
+
+	/**
+	 * @param figure
+	 * @return the bounds of the given figure relative to this figure's bounds
+	 */
+	private Rectangle getLocalBounds(IFigure figure) {
+
+		Rectangle figureBounds = figure.getBounds().getCopy();
+		figure.translateToAbsolute(figureBounds);
+		translateToRelative(figureBounds);
+		return figureBounds;
+
+	}
+
+	/**
+	 * sets the move indicator direction.
+	 * 
+	 * @param moveDirection
+	 *            the direction vector of the move direction or null if no move
+	 *            should be shown
+	 */
+	public void setMoveDirection(Vector moveDirection) {
+		if (!isInitialized()) {
+			initialize();
+		}
+		this.moveDirection = moveDirection;
+		if (moveDirection != null) {
+			directionIndicator.setVisible(true);
+			directionIndicator.setDirection(moveDirection);
+		} else {
+			directionIndicator.setVisible(false);
+		}
+		applyChangeStyles();
+	}
+
+	/**
+	 * @return the direction of the move indicator or null if no direction is
+	 *         set.
+	 */
+	public Vector getMoveDirection() {
+		return moveDirection;
+	}
+
+	/**
+	 * @param changeType
+	 *            the change type which this overlay represents.
+	 */
+	public void setChangeType(ChangeType changeType) {
+		this.changeType = changeType;
+		applyChangeStyles();
+	}
+
+	/**
+	 * @param boundsWidthChangeType
+	 *            the change type to display for the width of the bounds
+	 */
+	public void setBoundsWidthChangeType(DimensionPropertyChangeType boundsWidthChangeType) {
+		this.boundsWidthChangeType = boundsWidthChangeType;
+		updateDimensionPropertyChangeIndicators();
+	}
+
+	/**
+	 * 
+	 * @param boundsHeightChangeType
+	 *            the change type to display for the height of the bounds
+	 */
+	public void setBoundsHeightChangeType(DimensionPropertyChangeType boundsHeightChangeType) {
+		this.boundsHeightChangeType = boundsHeightChangeType;
+		updateDimensionPropertyChangeIndicators();
+	}
+
+	/**
+	 * @return the change type which this overlay represents.
+	 */
+	public ChangeType getChangeType() {
+		return changeType;
+	}
+
+	/**
+	 * Base interface for style advisors used to obtain a style for a given
+	 * {@link ChangeType}.
+	 * 
+	 * @author Florian Zoubek
+	 *
+	 */
+	public static interface IChangeTypeStyleAdvisor {
+
+		public Color getForegroundColorForChangeType(ChangeType changeType);
+
+		public Color getBackgroundColorForChangeType(ChangeType changeType);
+
+		public Color getIndicatorColorForChangeType(ChangeType changeType);
+
+		public Color getCommentColorForChangeType(ChangeType changeType);
+
+	}
+
+	/**
+	 * The default style advisor for changes.
+	 * 
+	 * @author Florian Zoubek
+	 *
+	 */
+	public static class DefaultChangeTypeStyleAdvisor implements IChangeTypeStyleAdvisor {
+
+		private Color additionForegroundColor;
+		private Color deletionForegroundColor;
+		private Color modificationForegroundColor;
+		private Color layoutForegroundColor;
+		private Color defaultForegroundColor;
+
+		private Color additionBackgroundColor;
+		private Color deletionBackgroundColor;
+		private Color modificationBackgroundColor;
+		private Color layoutBackgroundColor;
+		private Color defaultBackgroundColor;
+
+		private Color commentsColor;
+
+		public DefaultChangeTypeStyleAdvisor() {
+
+			additionForegroundColor = new Color(Display.getDefault(), 82, 145, 102);
+			additionBackgroundColor = new Color(Display.getDefault(), 82, 145, 102);
+
+			deletionForegroundColor = new Color(Display.getDefault(), 153, 153, 153);
+			deletionBackgroundColor = new Color(Display.getDefault(), 153, 153, 153);
+
+			modificationForegroundColor = new Color(Display.getDefault(), 31, 120, 180);
+			modificationBackgroundColor = new Color(Display.getDefault(), 31, 120, 180);
+
+			layoutForegroundColor = new Color(Display.getDefault(), 178, 223, 138);
+			layoutBackgroundColor = new Color(Display.getDefault(), 178, 223, 138);
+
+			commentsColor = new Color(Display.getDefault(), 247, 177, 0);
+
+			defaultForegroundColor = ColorConstants.black;
+			defaultBackgroundColor = ColorConstants.white;
+
+		}
+
+		public void dispose() {
+
+			additionForegroundColor.dispose();
+			additionBackgroundColor.dispose();
+
+			deletionForegroundColor.dispose();
+			deletionBackgroundColor.dispose();
+
+			modificationForegroundColor.dispose();
+			modificationBackgroundColor.dispose();
+			commentsColor.dispose();
+		}
+
+		@Override
+		public Color getForegroundColorForChangeType(ChangeType changeType) {
+
+			switch (changeType) {
+			case ADDITION:
+				return additionForegroundColor;
+			case DELETION:
+				return deletionForegroundColor;
+			case MODIFICATION:
+				return modificationForegroundColor;
+			case LAYOUT:
+				return layoutForegroundColor;
+			}
+			return defaultForegroundColor;
+
+		}
+
+		@Override
+		public Color getBackgroundColorForChangeType(ChangeType changeType) {
+
+			switch (changeType) {
+			case ADDITION:
+				return additionBackgroundColor;
+			case DELETION:
+				return deletionBackgroundColor;
+			case MODIFICATION:
+				return modificationBackgroundColor;
+			case LAYOUT:
+				return layoutBackgroundColor;
+			}
+			return defaultBackgroundColor;
+
+		}
+
+		@Override
+		public Color getCommentColorForChangeType(ChangeType changeType) {
+			return commentsColor;
+		}
+
+		@Override
+		public Color getIndicatorColorForChangeType(ChangeType changeType) {
+			return ColorConstants.black;
+		}
+
+	}
+
+	/**
+	 * @param showCommentsHint
+	 *            true if a comment hint should be shown, false otherwise
+	 */
+	public void setShowCommentHint(boolean showCommentsHint) {
+		this.showCommentHint = showCommentsHint;
+		updateOutline();
+	}
+}
---
+++ plugins/at.bitandart.zoubek.mervin.draw2d/src/at/bitandart/zoubek/mervin/draw2d/figures/examples/ChangeOverlayNodeFiguresExample.java
@@ -0,0 +1,231 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Florian Zoubek.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Florian Zoubek - initial API and implementation
+ *******************************************************************************/
+package at.bitandart.zoubek.mervin.draw2d.figures.examples;
+
+import org.eclipse.draw2d.DelegatingLayout;
+import org.eclipse.draw2d.GridData;
+import org.eclipse.draw2d.GridLayout;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.Label;
+import org.eclipse.draw2d.Layer;
+import org.eclipse.draw2d.LayeredPane;
+import org.eclipse.draw2d.LineBorder;
+import org.eclipse.draw2d.MarginBorder;
+import org.eclipse.draw2d.geometry.Vector;
+
+import at.bitandart.zoubek.mervin.draw2d.figures.ChangeOverlayNodeFigure;
+import at.bitandart.zoubek.mervin.draw2d.figures.ChangeOverlayNodeFigure.DimensionPropertyChangeType;
+import at.bitandart.zoubek.mervin.draw2d.figures.ChangeOverlayNodeFigure.ChangeType;
+import at.bitandart.zoubek.mervin.draw2d.figures.ChangeOverlayNodeFigure.DefaultChangeTypeStyleAdvisor;
+import at.bitandart.zoubek.mervin.draw2d.figures.DefaultOverlayLocator;
+import at.bitandart.zoubek.mervin.draw2d.figures.OverlayLinkedFigureListener;
+
+/**
+ * Demonstrates the usage of {@link ChangeOverlayNodeFigure}.
+ * 
+ * @author Florian Zoubek
+ *
+ */
+public class ChangeOverlayNodeFiguresExample extends BaseExample {
+
+	private static final String PRIMARY_LAYER = "primary layer";
+	private static final String OVERLAY_LAYER = "overlay layer";
+	private DefaultChangeTypeStyleAdvisor styleAdvisor;
+
+	public static void main(String[] args) {
+		new ChangeOverlayNodeFiguresExample().run();
+	}
+
+	@Override
+	protected IFigure createRootFigure() {
+
+		LayeredPane layeredPane = new LayeredPane();
+
+		/*
+		 * the primary layer where all linked figures reside
+		 */
+
+		Layer primaryLayer = new Layer();
+		primaryLayer.setOpaque(true);
+		primaryLayer.setBackgroundColor(org.eclipse.draw2d.ColorConstants.white);
+		GridLayout gridLayout = new GridLayout(10, true);
+		gridLayout.horizontalSpacing = 75;
+		gridLayout.verticalSpacing = 75;
+		primaryLayer.setLayoutManager(gridLayout);
+		primaryLayer.setBorder(new MarginBorder(100));
+		layeredPane.add(primaryLayer, PRIMARY_LAYER);
+
+		/*
+		 * Overlay Figures should always be placed on a overlay layer with a
+		 * delegating layout (or any layout that calls
+		 * IOverlayFigure#updateBoundsfromLinkedFigures()) that is placed above
+		 * the layer with the linked figures.
+		 */
+
+		Layer overlayLayer = new Layer();
+		overlayLayer.setLayoutManager(new DelegatingLayout());
+		layeredPane.add(overlayLayer, OVERLAY_LAYER);
+
+		return layeredPane;
+	}
+
+	@Override
+	protected void addChildFigures(IFigure parentFigure) {
+
+		if (parentFigure instanceof LayeredPane) {
+
+			LayeredPane layeredPane = (LayeredPane) parentFigure;
+			Layer primaryLayer = layeredPane.getLayer(PRIMARY_LAYER);
+			Layer overlayLayer = layeredPane.getLayer(OVERLAY_LAYER);
+
+			styleAdvisor = new DefaultChangeTypeStyleAdvisor();
+
+			OverlayLinkedFigureListener overlayLinkedFigureListener = new OverlayLinkedFigureListener(overlayLayer);
+
+			for (ChangeType changeType : ChangeType.values()) {
+
+				/* add the linked figures to the primary layer */
+
+				Label label = new Label("figure");
+				label.addFigureListener(overlayLinkedFigureListener);
+				label.setBorder(new LineBorder());
+				primaryLayer.add(label, getGridDataConstraint());
+
+				Label labelWithComments = new Label("figure");
+				labelWithComments.addFigureListener(overlayLinkedFigureListener);
+				labelWithComments.setBorder(new LineBorder());
+				primaryLayer.add(labelWithComments, getGridDataConstraint());
+
+				Label widthResizedLabel = new Label("figure");
+				widthResizedLabel.addFigureListener(overlayLinkedFigureListener);
+				widthResizedLabel.setBorder(new LineBorder());
+				primaryLayer.add(widthResizedLabel, getGridDataConstraint());
+
+				Label heightResizedLabel = new Label("figure");
+				heightResizedLabel.addFigureListener(overlayLinkedFigureListener);
+				heightResizedLabel.setBorder(new LineBorder());
+				primaryLayer.add(heightResizedLabel, getGridDataConstraint());
+
+				Label resizedLabel = new Label("figure");
+				resizedLabel.addFigureListener(overlayLinkedFigureListener);
+				resizedLabel.setBorder(new LineBorder());
+				primaryLayer.add(resizedLabel, getGridDataConstraint());
+
+				Label resizedLabelWithComments = new Label("figure");
+				resizedLabelWithComments.addFigureListener(overlayLinkedFigureListener);
+				resizedLabelWithComments.setBorder(new LineBorder());
+				primaryLayer.add(resizedLabelWithComments, getGridDataConstraint());
+
+				Label movedLabel = new Label("figure");
+				movedLabel.addFigureListener(overlayLinkedFigureListener);
+				movedLabel.setBorder(new LineBorder());
+				primaryLayer.add(movedLabel, getGridDataConstraint());
+
+				Label movedLabelWithComments = new Label("figure");
+				movedLabelWithComments.addFigureListener(overlayLinkedFigureListener);
+				movedLabelWithComments.setBorder(new LineBorder());
+				primaryLayer.add(movedLabelWithComments, getGridDataConstraint());
+
+				Label movedResizedLabel = new Label("figure");
+				movedResizedLabel.addFigureListener(overlayLinkedFigureListener);
+				movedResizedLabel.setBorder(new LineBorder());
+				primaryLayer.add(movedResizedLabel, getGridDataConstraint());
+
+				Label movedResizedLabelWithComments = new Label("figure");
+				movedResizedLabelWithComments.addFigureListener(overlayLinkedFigureListener);
+				movedResizedLabelWithComments.setBorder(new LineBorder());
+				primaryLayer.add(movedResizedLabelWithComments, getGridDataConstraint());
+
+				/*
+				 * Add the overlay figures to the overlay layer. In this case
+				 * overlay figures are linked to other figures on the primary
+				 * layer using the DefaultOverlayLocator. This locator simply
+				 * delegates the bounds update command to
+				 * IOverlayFigure#updateBoundsfromLinkedFigures(). Notice that
+				 * the following figures demonstrate the usage but do not
+				 * represent all possible variations.
+				 */
+
+				ChangeOverlayNodeFigure simpleOverlay = new ChangeOverlayNodeFigure(styleAdvisor, changeType);
+				overlayLayer.add(simpleOverlay, new DefaultOverlayLocator(label));
+
+				ChangeOverlayNodeFigure overlayWithComments = new ChangeOverlayNodeFigure(styleAdvisor, changeType);
+				overlayWithComments.setShowCommentHint(true);
+				overlayLayer.add(overlayWithComments, new DefaultOverlayLocator(labelWithComments));
+
+				ChangeOverlayNodeFigure overlayWithWidthResize = new ChangeOverlayNodeFigure(styleAdvisor, changeType);
+				overlayWithWidthResize.setBoundsWidthChangeType(DimensionPropertyChangeType.SMALLER);
+				overlayLayer.add(overlayWithWidthResize, new DefaultOverlayLocator(widthResizedLabel));
+
+				ChangeOverlayNodeFigure overlayWithHeightResize = new ChangeOverlayNodeFigure(styleAdvisor, changeType);
+				overlayWithHeightResize.setBoundsHeightChangeType(DimensionPropertyChangeType.SMALLER);
+				overlayLayer.add(overlayWithHeightResize, new DefaultOverlayLocator(heightResizedLabel));
+
+				ChangeOverlayNodeFigure overlayWithResize = new ChangeOverlayNodeFigure(styleAdvisor, changeType);
+				overlayWithResize.setBoundsHeightChangeType(DimensionPropertyChangeType.SMALLER);
+				overlayWithResize.setBoundsWidthChangeType(DimensionPropertyChangeType.BIGGER);
+				overlayLayer.add(overlayWithResize, new DefaultOverlayLocator(resizedLabel));
+
+				ChangeOverlayNodeFigure overlayWithCommentsResize = new ChangeOverlayNodeFigure(styleAdvisor,
+						changeType);
+				overlayWithCommentsResize.setBoundsHeightChangeType(DimensionPropertyChangeType.SMALLER);
+				overlayWithCommentsResize.setBoundsWidthChangeType(DimensionPropertyChangeType.SMALLER);
+				overlayWithCommentsResize.setShowCommentHint(true);
+				overlayLayer.add(overlayWithCommentsResize, new DefaultOverlayLocator(resizedLabelWithComments));
+
+				ChangeOverlayNodeFigure overlayWithMove = new ChangeOverlayNodeFigure(styleAdvisor, changeType);
+				overlayWithMove.setMoveDirection(new Vector(0, 1));
+				overlayLayer.add(overlayWithMove, new DefaultOverlayLocator(movedLabel));
+
+				ChangeOverlayNodeFigure overlayWithMoveResize = new ChangeOverlayNodeFigure(styleAdvisor, changeType);
+				overlayWithMoveResize.setBoundsHeightChangeType(DimensionPropertyChangeType.BIGGER);
+				overlayWithMoveResize.setBoundsWidthChangeType(DimensionPropertyChangeType.SMALLER);
+				overlayWithMoveResize.setMoveDirection(new Vector(1, 1));
+				overlayLayer.add(overlayWithMoveResize, new DefaultOverlayLocator(movedResizedLabel));
+
+				ChangeOverlayNodeFigure overlayWithCommentsMove = new ChangeOverlayNodeFigure(styleAdvisor, changeType);
+				overlayWithCommentsMove.setShowCommentHint(true);
+				overlayWithCommentsMove.setMoveDirection(new Vector(-1, 0.5));
+				overlayLayer.add(overlayWithCommentsMove, new DefaultOverlayLocator(movedLabelWithComments));
+
+				ChangeOverlayNodeFigure overlayWithCommentsMoveResize = new ChangeOverlayNodeFigure(styleAdvisor,
+						changeType);
+				overlayWithCommentsMoveResize.setShowCommentHint(true);
+				overlayWithCommentsMoveResize.setBoundsHeightChangeType(DimensionPropertyChangeType.SMALLER);
+				overlayWithCommentsMoveResize.setBoundsWidthChangeType(DimensionPropertyChangeType.BIGGER);
+				overlayWithCommentsMoveResize.setMoveDirection(new Vector(-1, 0.5));
+				overlayLayer.add(overlayWithCommentsMoveResize,
+						new DefaultOverlayLocator(movedResizedLabelWithComments));
+
+			}
+
+		}
+	}
+
+	/**
+	 * @return the default grid data constraint for figures on the primary
+	 *         layer.
+	 */
+	private GridData getGridDataConstraint() {
+		GridData gridData = new GridData(GridData.FILL, GridData.FILL, false, false);
+		gridData.heightHint = 50;
+		gridData.widthHint = 75;
+		return gridData;
+	}
+
+	@Override
+	protected void cleanUp() {
+		if (styleAdvisor != null) {
+			styleAdvisor.dispose();
+		}
+	}
+
+}
