e9721fe3a743d543eea105ca472569b7a234e6a2
Florian Zoubek <zoubek@bitandart.at>
2016-02-24 14:22:36 +0100

    adds toolbar with items intended for toggling change overlays
    - adds toolbar and commands for overlay toggles to the mervin fragment
    - adds command handler stubs that should toggle addition, deletion, 
      modification, and layout overlays in the future.
    - adds toolbar icons for overlay toggles
    - adds vector graphics source of the overlay toggle toolbar icons

---
+++ plugins/at.bitandart.zoubek.mervin/src/at/bitandart/zoubek/mervin/handlers/ToggleAdditionOverlays.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Florian Zoubek.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Florian Zoubek - initial API and implementation
+ *******************************************************************************/
+package at.bitandart.zoubek.mervin.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.menu.MHandledToolItem;
+
+/**
+ * Handles hide/show request for addition overlays.
+ * 
+ * @author Florian Zoubek
+ *
+ */
+public class ToggleAdditionOverlays {
+
+	@Execute
+	public void execute(MHandledToolItem item) {
+		// TODO
+	}
+
+}
