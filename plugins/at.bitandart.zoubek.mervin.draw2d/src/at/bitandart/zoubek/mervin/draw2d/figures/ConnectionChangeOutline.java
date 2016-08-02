ff39dd83c655b97bb6e1198095d756a517c2a820
Florian Zoubek <zoubek@bitandart.at>
2016-08-02 15:04:31 +0200

    fixes selection of connection overlays caused by incorrect bounds

--- plugins/at.bitandart.zoubek.mervin.draw2d/src/at/bitandart/zoubek/mervin/draw2d/figures/ConnectionChangeOutline.java
+++ plugins/at.bitandart.zoubek.mervin.draw2d/src/at/bitandart/zoubek/mervin/draw2d/figures/ConnectionChangeOutline.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Florian Zoubek.
+ * Copyright (c) 2015, 2016 Florian Zoubek.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -64,7 +64,7 @@ public class ConnectionChangeOutline extends Shape {
 
 	@Override
 	public Rectangle getBounds() {
-		return pointsToCover.getBounds().expand(paddingWidth, paddingWidth);
+		return pointsToCover.getBounds().getCopy().expand(paddingWidth, paddingWidth);
 	}
 
 	@Override
