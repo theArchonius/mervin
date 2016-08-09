c5903a3d1d34e3754a9307ca5a42cc7adb3ab5a0
Florian Zoubek <zoubek@bitandart.at>
2016-08-09 11:33:55 +0200

    adapts WorkspaceEditPart to register child workbench containers properly

--- plugins/at.bitandart.zoubek.mervin/src/at/bitandart/zoubek/mervin/diagram/diff/parts/WorkspaceEditPart.java
+++ plugins/at.bitandart.zoubek.mervin/src/at/bitandart/zoubek/mervin/diagram/diff/parts/WorkspaceEditPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Florian Zoubek.
+ * Copyright (c) 2015, 2016 Florian Zoubek.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,6 +39,7 @@ import at.bitandart.zoubek.mervin.draw2d.PluginMervinResourceRegistry;
 import at.bitandart.zoubek.mervin.draw2d.RegistryResourceManager;
 import at.bitandart.zoubek.mervin.draw2d.figures.workbench.DiffWorkbench;
 import at.bitandart.zoubek.mervin.draw2d.figures.workbench.IDiffWorkbench;
+import at.bitandart.zoubek.mervin.draw2d.figures.workbench.IDiffWorkbenchContainer;
 import at.bitandart.zoubek.mervin.model.modelreview.ModelReview;
 
 /**
@@ -83,6 +84,20 @@ public class WorkspaceEditPart extends ShapeNodeEditPart implements IPrimaryEdit
 			return ((IDiffWorkbench) figure).getContentArea();
 		}
 		return figure;
+	}
+
+	@Override
+	protected void addChildVisual(EditPart childEditPart, int index) {
+
+		super.addChildVisual(childEditPart, index);
+
+		if (childEditPart instanceof GraphicalEditPart) {
+
+			IFigure childFigure = ((GraphicalEditPart) childEditPart).getFigure();
+			if (childFigure instanceof IDiffWorkbenchContainer && figure instanceof IDiffWorkbench) {
+				((IDiffWorkbench) figure).registerContainer((IDiffWorkbenchContainer) childFigure);
+			}
+		}
 	}
 
 	@Override
